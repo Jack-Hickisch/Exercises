6cfb731f675ea654720a374afb5db9e4d6239b61
Jack-Hickisch <jackhickisch@gmail.com>
2019-08-23 18:56:08 -0600

    HW 8-23-2019 Ch 11 Complete

---
+++ chapter11/ObjectCreatedAtRunTime.java
@@ -0,0 +1,11 @@
+public class ObjectCreatedAtRunTime
+{
+    public static void main( String[] args )
+    {
+        String str = new String("Hello World");
+        String secondVariable = ("Hello????"); // is this what it was aksing???
+
+        System.out.println( str );
+        System.out.println( secondVariable );
+    }
+}
---
+++ chapter11/StringThatCreatesANewString.java
@@ -0,0 +1,12 @@
+public class StringThatCreatesANewString
+{
+    public static void main ( String[] args )
+    {
+        String first = new String("     In a Hole in the ground there lived a Hobbit.   ");
+        String second;
+
+        second = first.trim();
+        System.out.println( second );
+
+    }
+}
