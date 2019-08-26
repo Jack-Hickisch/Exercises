508bc781546f2348a0269c4574a0950ec34cfa6c
Jack Hickisch <jackhickisch@gmail.com>
2019-08-26 12:35:37 -0600

    Echo Square working

--- Chapter12/EchoSquare.java
+++ Chapter12/EchoSquare.java
@@ -4,6 +4,13 @@ public class EchoSquare
 {
     public static void main (String[] args)
     {
+        Scanner scan = new Scanner( System.in );
+        int num, square;      // declare two int variable
         
+        System.out.println("Enter an integer:");
+        num = scan.nextInt();
+        square = num * num;     // compute the square
+
+        System.out.println("The square of " + num + " is " + square);
     }
 }
