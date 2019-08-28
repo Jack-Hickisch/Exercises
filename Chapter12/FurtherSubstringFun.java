12e20c59f869cebe4b7abce9f0afe2b8705a6a1d
Jack-Hickisch <jackhickisch@gmail.com>
2019-08-27 18:15:50 -0600

    Further Substring Fun working

--- Chapter12/FurtherSubstringFun.java
+++ Chapter12/FurtherSubstringFun.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class FurtherSubstringFun
 {
     public static void main(String[] args)
     {
+        String str, substring, begin, end;
+        Scanner scan = new Scanner( System.in );
 
+        System.out.println("Enter your string: ");
+        str = scan.nextLine();
+
+        System.out.println("Enter the start index: ");
+        begin = scan.nextLine();
+
+        System.out.println("Enter the end index: ");
+        end = scan.nextLine();
+
+        substring = str.substring( Integer.valueOf(begin), Integer.valueOf(end) );
+
+        System.out.println("Original String:" + str);
+        System.out.println("Substring: " + substring);
     }
 }
--- ObjectCreatedAtRunTime.java
+++
@@ -1,9 +0,0 @@
-public class ObjectCreatedAtRunTime
-{
-    public static void main( String[] args )
-    {
-        String str = new String("Hello World")
-
-        S
-    }
-}
