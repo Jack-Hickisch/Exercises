1560ec5499cef8ca18dc4e48aee77bbfe2be085d
Jack-Hickisch <jackhickisch@gmail.com>
2019-09-14 19:35:32 -0600

    ch 20 done

--- Chapter20/HarmonicSum.java
+++ Chapter20/HarmonicSum.java
@@ -5,5 +5,17 @@ public class HarmonicSum
     public static void main( String[] args)
     {
         Scanner scan = new Scanner( System.in );
+        double n, sum = 0, i = 1;
+
+        System.out.println("Enter N");
+        n = scan.nextDouble();
+
+        while (n >= i)
+        {
+            sum = sum + (1.0 / i);
+            i++;
+        }
+        
+        System.out.println("Sum is: " + sum);
     }
 }
