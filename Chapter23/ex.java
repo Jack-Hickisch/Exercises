296eb35e1bd84278349ee13acf5ff2b74ffc40cd
Jack Hickisch <jackhickisch@gmail.com>
2019-09-17 12:52:23 -0600

    ex setup

---
+++ Chapter23/ex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        double x, e = Math.exp(1);
+
+        System.out.println("Enter x: ");
+        x = scan.nextDouble();
+
+        
+    }
+}
