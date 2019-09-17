21077e64fb2357b59b56209a57199342ee2c6dac
Jack Hickisch <jackhickisch@gmail.com>
2019-09-17 12:11:12 -0600

    21 done

--- Chapter21/MultiplicationQuiz.java
+++ Chapter21/MultiplicationQuiz.java
@@ -7,12 +7,12 @@ public class MultiplicationQuiz
     {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
-        int num1, num2, proposed_ans;
+        int num1, num2, proposed_ans, q_count = 0;
 
         for (int i = 1; i < 10; i++)
         {
-            num1 = random.nextInt();
-            num2 = random.nextInt();
+            num1 = random.nextInt(10);
+            num2 = random.nextInt(10);
 
             System.out.println("What is " + num1 + " * " + num2);
             proposed_ans = scan.nextInt();
@@ -20,11 +20,14 @@ public class MultiplicationQuiz
             if (num1 * num2 == proposed_ans)
             {
                 System.out.println("Right!");
+                q_count += 1;
             }
             else
             {
                 System.out.println("Wrong!");
             }
         }
+
+        System.out.println("You got " + q_count + " out of 10 questions correct!");
     }
 }
