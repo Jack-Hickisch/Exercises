14c0a79c02b9cfbb1779cd422fd3b7a0e4b2ab7c
Jack Hickisch <jackhickisch@gmail.com>
2019-10-02 07:46:42 -0600

    Workign on 30 cont.

--- Chapter30/PasswordCracker.java
+++ Chapter30/PasswordCracker.java
@@ -6,20 +6,36 @@ public class PasswordCracker
     {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
-        String real_pswrd = "", temp_pswrd = "", chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        String real_pswrd = "", temp_pswrd = "", chars = "qwertyuiopasdfghjklzxcvbnm";
         boolean guess = false;
+        int rounds, count = 0;
 
         System.out.println("Enter a secret password -->");
         real_pswrd = scan.nextLine();
 
-        for (int i = 0; !guess; i++)
+        if (real_pswrd.length() > 5)
         {
-            for (int j = 0; j < 5; j++)
+            System.out.println("Invalid Length; press Ctrl + C to exit");
+        }
+
+        while (!guess)
+        {
+            rounds = rand.nextInt(5) + 1;
+
+            for (int i = 0; i < rounds; i++);
             {
-                temp_pswrd += chars.substring(i, i + 1);
+                int random_place = rand.nextInt(26);
+                temp_pswrd += chars.substring(random_place, random_place + 1);
+                count++;
                 System.out.println(temp_pswrd);
             }
             temp_pswrd = "";
+        }
+        
+        if (temp_pswrd.equals(real_pswrd))
+        {
+            guess = true;
+            System.out.println("It took " + count + " tries to guess your password");
         }
     }
 }
