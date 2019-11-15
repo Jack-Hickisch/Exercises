ae8977e3af4807a1c99db6c17d9467094ee38464
Jack Hickisch <jackhickisch@gmail.com>
2019-11-15 12:53:17 -0700

    54 created

---
+++ chapter54/OdiousNumbers.java
@@ -0,0 +1,16 @@
+public class OdiousNumbers
+{
+    Public Static void main (String args[])
+    {
+
+    }
+
+    public boolean OdiousTF(number)
+    {
+        int currentNumber = number;
+        for (boolean done = false;; done)
+        {
+            
+        }
+    }
+}
