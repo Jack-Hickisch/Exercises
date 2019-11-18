56c9329ba5773c7dc2f46f47e462c29056933966
Jack Hickisch <jackhickisch@gmail.com>
2019-11-18 12:52:04 -0700

    odious numbers done

--- chapter54/OdiousNumbers.java
+++ chapter54/OdiousNumbers.java
@@ -1,16 +1,63 @@
+import java.util.Scanner;
+
 public class OdiousNumbers
 {
-    Public Static void main (String args[])
+    public static void main (String args[])
     {
+        long number;
+        Scanner scan = new Scanner ( System.in );
 
+        for (boolean done = false; !done ;)
+        {
+            System.out.println("Enter your number");
+            number = scan.nextLong();
+
+            if (number >= 0)
+            {
+                if (odiousTF(number))
+                {
+                    System.out.println("is odious");
+                }
+                else
+                {
+                   System.out.println("is not odious");
+                }
+                System.out.println("");
+                System.out.println("");
+            }
+            else
+            {
+                done = true;
+            }
+        }
     }
 
-    public boolean OdiousTF(number)
+    public static boolean odiousTF(long number)
     {
-        int currentNumber = number;
-        for (boolean done = false;; done)
+        long currentNumber = number;
+        long oneCount = 0;
+
+        for (boolean done = false; !done ;)
         {
+            if (currentNumber % 2 == 1)
+            {
+                oneCount += 1;
+            }
             
+            currentNumber /= 2;
+
+            if (currentNumber == 0)
+            {
+                done = true;
+                if (oneCount % 2 != 0)
+                {
+                    return true;
+                    // System.out.println(oneCount);
+                }
+                return false;
+            }
         }
+
+        return false;
     }
 }
